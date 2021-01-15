21bf26ba00a8f3f7eeafaed2139dad8bcef5ca6c
Seth Staggs <sethstaggs2@gmail.com>
2021-01-15 14:12:36 -0600

    made schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Burger King Foot Lettuce');
+INSERT INTO burgers (burger_name) VALUE ('Mushroom & Swiss');
+INSERT INTO burgers (burger_name) VALUE ('Double Bacon Cheeseburger');
